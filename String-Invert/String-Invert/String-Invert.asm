2ce670a56748b81c618e2de28d3c561d5f41c897
Mateusz Bobiński <mat.bobinski@gmail.com>
2013-11-12 22:14:16 +0100

    String-Invert

--- String-Invert/String-Invert/String-Invert.asm
+++ String-Invert/String-Invert/String-Invert.asm
@@ -10,17 +10,17 @@
 	LDI R16,high(RAMEND)	
 	OUT SPH, R16
 
-	//Miejsce w którym odwrócony napis bêdzie przechowywany
+	//Miejsce w ktorym odwrocony napis bedzie przechowywany
 	LDI XL, 0x00
 	LDI XH, 0x01
-	//Miejsce w pamiêci flash w którym napis jest zapisany
+	//Miejsce w pamieci flash w ktorym napis jest zapisany
 	LDI ZL, low(2 * NAPIS)
 	LDI ZH, high(2 * NAPIS)
 
 	.def tmp = R17
 	LDI tmp, 0x00
 	
-	// Zachowanie stanu rejestrów na stosie
+	// Zachowanie stanu rejestrow na stosie
 	PUSH XL
 	PUSH XH
 	PUSH ZL
@@ -28,7 +28,7 @@
 	//Wrzuceni pomocniczego 0
 	PUSH tmp
 	
-	//Wrzucanie na stos kolejnych liter slowa, a¿ do otrzymania 0
+	//Wrzucanie na stos kolejnych liter slowa, az do otrzymania 0
 	INVERT:
 	LPM tmp, Z+ 
 	CPI tmp, 0
@@ -36,7 +36,7 @@
 	PUSH tmp
 	JMP INVERT
 
-	//Œci¹ganie kolejnych liter s³owa, a¿ do otrzymania 0, które by³o wrzucone jako pomocnicze przed pêtl¹ INVERT
+	//Sciaganie kolejnych liter slowa, az do otrzymania 0, ktore bylo wrzucone jako pomocnicze przed petla INVERT
 	COPY:
 	POP tmp
 	CPI tmp, 0
@@ -45,7 +45,7 @@
 	JMP COPY
 
 	END:
-	//Odzyskanie stanu rejestrów
+	//Odzyskanie stanu rejestrow
 	POP ZH
 	POP ZL
 	POP XH
